d650211f601e5bcf11239bf8973d481a1c2d40c9
yoneda <nsshowdream@gmail.com>
2020-11-13 18:37:58 +0900

    [fix] add a styled textarea component

--- Editor.js
+++ Editor.js
@@ -11,15 +11,24 @@ import {
   useStoreActions,
 } from "easy-peasy";
 
-const Box = styled.div`
+const Wrapper = styled.div`
   width: 200px;
   height: 200px;
   border: solid 1px black;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+const EditArea = styled.textarea`
+  resize: none;
+  line-height: 1;
+  font-size: 2rem;
 `;
 
 function Editor() {
-  const selected = useStoreState((state) => state.selected);
-  return <Box>{selected && selected.body}</Box>;
+  // const selected = useStoreState((state) => state.selected);
+  // return <Box>{selected && selected.body}</Box>;
+  return <Wrapper><EditArea cols={16} rows={4} /></Wrapper>
 }
 
 export default Editor;
