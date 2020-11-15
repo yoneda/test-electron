ee0ebf13e36692732967a7145613f52caf55d453
yoneda <nsshowdream@gmail.com>
2020-11-15 13:27:31 +0900

    [fix] make it possible to select a note

--- NoteList.js
+++ NoteList.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Reset } from "styled-reset";
 import dayjs from "dayjs";
 import {
@@ -11,57 +11,33 @@ import {
   useStoreActions,
 } from "easy-peasy";
 
-const Box = styled.div`
+const Wrapper = styled.div`
   width: 200px;
   height: 200px;
   border: solid 1px black;
+  box-sizing: border-box;
 `;
-/*
-const NoteBox = styled.div`
-  corsor: pointer;
-  &:hover {
-    background: whitesmoke;
-  }
-  &:active {
-    background: gainsboro;
-  }
-
-  ${(props) =>
-    props.light &&
-    css`
-      bakground: red;
-    `}
-`;
-
-function Note(props) {
-  const { body, light, onTap } = props;
-  return (
-    <NoteBox light={light} onClick={onTap}>
-      {body}
-    </NoteBox>
-  );
-}
-*/
 
 const Note = styled.div`
-  
+  cursor: pointer;
+  background: ${(props) => (props.light ? "whitesmoke" : "white")};
 `;
 
 function NoteList() {
   const [notes, index] = useStoreState((state) => [state.notes, state.index]);
   const tapNote = useStoreActions((actions) => actions.tapNote);
   return (
-    <Box>
+    <Wrapper>
       {notes.map((note, key) => (
-        <div
+        <Note
           key={key}
-          light={index}
-          onTap={() => tapNote({ id: note.id })}
+          onClick={() => tapNote({ id: note.id })}
+          light={key === index}
         >
           {note.body}
-        </div>
+        </Note>
       ))}
-    </Box>
+    </Wrapper>
   );
 }
 
