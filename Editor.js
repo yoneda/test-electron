65c34f3daa16fe16d7fd616b42129907d182c9a2
yoneda <nsshowdream@gmail.com>
2020-11-16 13:53:05 +0900

    [fix] add a trash icon and can remove a note

--- Editor.js
+++ Editor.js
@@ -25,18 +25,59 @@ const EditArea = styled.textarea`
   font-size: 1.5rem;
 `;
 
+const Svg = styled.svg`
+  cursor: pointer;
+  &:hover {
+    background-color: whitesmoke;
+  }
+  &:active {
+    background-color: gainsboro;
+  }
+`;
+
+function Trash() {
+  return (
+    <Svg
+      className="icon icon-tabler icon-tabler-trash"
+      height="24"
+      width="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+      <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+      <line x1="4" x2="20" y1="7" y2="7" />
+      <line x1="10" x2="10" y1="11" y2="17" />
+      <line x1="14" x2="14" y1="11" y2="17" />
+    </Svg>
+  );
+}
+
 function Editor() {
   const note = useStoreState((state) => state.selected);
-  const editNote = useStoreActions((actions) => actions.editNote);
+  const [editNote, removeNote] = useStoreActions((actions) => [
+    actions.editNote,
+    actions.removeNote,
+  ]);
   return (
     <Wrapper>
       {note ? (
-        <EditArea
-          cols={12}
-          rows={4}
-          value={note.body}
-          onChange={(e) => editNote({ id: note.id, body: e.target.value })}
-        />
+        <Fragment>
+          <EditArea
+            cols={12}
+            rows={4}
+            value={note.body}
+            onChange={(e) => editNote({ id: note.id, body: e.target.value })}
+          />
+          <span onClick={() => removeNote({ id: note.id })}>
+            <Trash />
+          </span>
+        </Fragment>
       ) : (
         "empty"
       )}
